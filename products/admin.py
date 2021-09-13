e1283b326db1d4b273581441736a611f63f33e20
hddahdda <hedda9@hotmail.com>
2021-09-13 13:35:41 +0000

    Customized admin for Product, Category and Image model

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 from .models import Product, Category, Image
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Image)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'friendly_name',
+        'name',
+        ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'category',
+        'friendly_name',
+        'sku',
+        'price',
+        'description',
+        'product_image'
+    ]
+    list_filter = ('category',)
+    ordering = ('sku',)
+
+
+class AdminImage(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'image',
+        'product'
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Image, AdminImage)
