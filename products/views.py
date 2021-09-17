07a49b929e465c93c7b4f260f90edb90dada091c
hddahdda <hedda9@hotmail.com>
2021-09-17 13:27:20 +0000

    added image view, updated product detail

--- products/views.py
+++ products/views.py
@@ -20,14 +20,35 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    # images = Image.objects.filter(product_id)
 
     context = {
         'product': product,
+        # 'images': images,
     }
 
     return render(request, 'products/product_detail.html', context)
 
-# def view_image(request, id):
-#     product = get_object_or_404(Product, id=id)
-#     images = Image.objects.filter(product=product)
-#     return 
+
+# def all_images(request, product_id):
+
+#     product_image = get_object_or_404(Product, pk=product_id)                   
+#     detailed_images = image_set.all() 
+
+#     context = {
+#         'product_images': product_images,
+#         'detailed_images': detailed_images,
+#     }
+
+#     return render(request, 'products/product_detail.html', context)
+
+
+def view_image(request, id):
+    product = get_object_or_404(Product, id=id)
+    images = Image.objects.filter(product=product)
+
+    context = {
+        'images': images,
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
