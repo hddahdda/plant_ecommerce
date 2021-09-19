f1a9cbb4c6238e3f7593b60d09845b8977dd7fda
hddahdda <hedda9@hotmail.com>
2021-09-19 10:14:35 +0000

    Added cart context processor + if statement in base.html to update cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contexts(request):
@@ -7,6 +9,18 @@ def cart_contexts(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- cart/views.py
+++ cart/views.py
@@ -11,7 +11,7 @@ def cart(request):
 
 def add_to_cart(request, item_id):
 
-    quantity = request.POST.get('quantity')
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
@@ -21,5 +21,4 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity 
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
