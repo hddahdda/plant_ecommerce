f727ba6290acc0425f59424156234437996cbfb6
hddahdda <hedda9@hotmail.com>
2021-09-20 10:43:15 +0000

    added toast templates for eventual use

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
+
+from products.models import Product
 
 # Create your views here.
 
@@ -26,25 +29,14 @@ def add_to_cart(request, item_id):
 
 def adjust_cart(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
+    cart = request.session.get("cart", {})
 
     if quantity > 0:
         cart[item_id] = quantity
     else:
-        cart.pop[item_id]
-
+        cart.pop(item_id)
+ 
     request.session['cart'] = cart
-    return redirect(reverse("cart"))
-
-
-def remove_from_cart(request, item_id):
-
-    try:
-        cart.pop[item_id]
-    
-    request.session['cart'] = cart
-    return HttpResponse(status=200)
-
-    except Exception as e:
-        return HttpResponse(status=500)
+    return redirect(reverse('cart'))
