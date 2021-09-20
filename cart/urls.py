c169570fdfc07da02028e60b0dab67d208ab1195
hddahdda <hedda9@hotmail.com>
2021-09-20 09:22:12 +0000

    updated product_detail + cart page to update quantity

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    path('add/<item_id>', views.add_to_cart, name="add_to_cart")
+    path('add/<item_id>', views.add_to_cart, name="add_to_cart"),
+    path('adjust/<item_id>', views.adjust_cart, name="adjust_cart"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -22,3 +22,29 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse("cart"))
+
+
+def remove_from_cart(request, item_id):
+
+    try:
+        cart.pop[item_id]
+    
+    request.session['cart'] = cart
+    return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
