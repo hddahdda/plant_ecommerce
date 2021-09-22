de08b15f9c45d7827757ec83eb437f4fb569a2a6
hddahdda <hedda9@hotmail.com>
2021-09-22 15:55:57 +0000

    Setup Stripe in webhooks.py, added webhooks listeners aswell as verification of webhooks

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
     """ Required for stripe webhooks """
 
     def __init__(self, request):
         self.request = request
 
-    def handle_event(self,event):
+    def handle_event(self, event):
         """
         Handle a unexpected or unknown webhook event
         """
