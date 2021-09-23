c24de5de8bffba45dfd01602ac4a131d84bab5cf
hddahdda <hedda9@hotmail.com>
2021-09-23 07:55:57 +0000

    Completed webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
 
 class StripeWH_Handler:
     """ Required for stripe webhooks """
@@ -27,8 +32,9 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.data.charges[0].amount / 100, 2)
-
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+  
+        # Clean data for shipping details
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
@@ -39,12 +45,15 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    email__iexact=shipping_details.email,
-                    phone_number__iexact=shipping_details.phone_number,
-                    country__iexact=shipping_details.country,
-                    postcode__iexact=shipping_details.postcode,
-                    street_address__iexact=shipping_details.street_address,
-                    grand_total__iexact=shipping_details.grand_total,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address__iexact=shipping_details.address.street_address,
+                    grand_total=grand_total,
+                    original_cart=cart,
+                    stripe_pid=pid,
                 )
                 order_exists = True
                 break
@@ -60,12 +69,14 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    email=shipping_details.email,
-                    phone_number=shipping_details.phone_number,
-                    country=shipping_details.country,
-                    postcode=shipping_details.postcode,
-                    street_address=shipping_details.street_address,
-                    grand_total=shipping_details.grand_total,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address=shipping_details.address.street_address,
+                    original_cart=cart,
+                    stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(cart).items():
                     product = Product.objects.get(id=item_id)
