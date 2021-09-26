a683bd3413f92112b99edbd91d477fa330ed82e3
hddahdda <hedda9@hotmail.com>
2021-09-26 08:17:02 +0000

    Added delete and edit item to all products page, set max-width and height to img

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.products_all, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>/', views.edit_product, name='edit_product')
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -68,9 +68,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added new product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add new product, recheck your form.')
     else:
@@ -107,3 +107,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product - admin """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
