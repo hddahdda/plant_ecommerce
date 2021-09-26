8183630aca3ed53da2d4dcade28d98d584046bc3
hddahdda <hedda9@hotmail.com>
2021-09-26 09:35:19 +0000

    Removed secret key + set up automatic deployment

--- plant_ecommerce/settings.py
+++ plant_ecommerce/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%n5*x_&-pe2!v1&qbboljtym%-u3ye_#ak==(#v^9#!&$kst3x'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['plantiq.herokuapp.com', 'localhost']
 
