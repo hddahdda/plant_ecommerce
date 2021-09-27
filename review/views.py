922c887c356631d375e0833897fddad8d2c14e32
hddahdda <hedda9@hotmail.com>
2021-09-27 21:54:33 +0000

    Removed commented out code

--- review/views.py
+++ review/views.py
@@ -1,43 +1,11 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 from .models import Review
 from .forms import FormReview
 from profiles.models import UserProfile
 from products.models import Product
 
-
-# @login_required
-# def add_review(request, product_id):
-
-#     product = Product.objects.get(pk=product_id)
-#     user = get_object_or_404(UserProfile, user=request.user)
-
-#     if request.method == 'POST':
-#         form = FormReview(request.POST)
-#         if form.is_valid():
-#             data = FormReview()
-#             data.subject = form.cleaned_data['subject']
-#             data.review = form.cleaned_data['review']
-#             data.product_id = product_id
-#             data.user = user
-#             data.save()
-#             return redirect(url)
-#         else:
-#             messages.error(request, 'There was an error with your form \
-#                 Check your information')
-#     else:
-#         form = FormReview()
-#     template = 'products/product_detail.html'
-#     context = {
-#         'form': form,
-#         'product': product,
-#     }
-#     return render(request, template, context)
-    
-    # product = get_object_or_404(Product, pk=product_id)
-    # user = get_object_or_404(UserProfile, user=request.user)
 
 @login_required
 def add_review(request, product_id):
@@ -57,4 +25,3 @@ def add_review(request, product_id):
             form = FormReview()
             template = 'products/product_detail.html'
         return render(request, template, {"form": form})
-
